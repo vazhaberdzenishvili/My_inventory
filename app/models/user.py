ad14f061bb90111863f3e42436454a820f78e544
vazhaberdzenishvili <79368740+vazhaberdzenishvili@users.noreply.github.com>
2021-06-13 14:58:29 +0400

    Update user.py

--- app/models/user.py
+++ app/models/user.py
@@ -46,15 +46,21 @@ class UserModel(db.Model, UserMixin):
     def delete_user(self):
         db.session.delete(self)
         db.session.commit()
+        
 
-
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(256), unique=True, nullable=False)
+    password_hash = db.Column(db.LargeBinary, nullable=True)
+    
+    
 class OAuth(OAuthConsumerMixin, db.Model):
     __table_args__ = (db.UniqueConstraint("provider", "provider_user_id"),)
     provider_user_id = db.Column(db.String(256), nullable=False)
     provider_user_login = db.Column(db.String(256), nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey(UserModel.id), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey(User.id), nullable=False)
     user = db.relationship(
-        UserModel,
+        User,
         # This `backref` thing sets up an `oauth` property on the User model,
         # which is a dictionary of OAuth models associated with that user,
         # where the dictionary key is the OAuth provider name.
